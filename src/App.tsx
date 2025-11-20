456d1c3b0832221c998599336c84a78047d6ca51
Kayanbarreto <kayan.marques.barreto@ccc.ufcg.edu.br>
2025-11-19 22:39:31 -0300

    chore: simplifica App.tsx para build em produção

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  return (
-    <div className="text-center p-6 bg-purple-100">
-      <h1 className="text-3xl font-bold">Agenda Web</h1>
-      <p className="mt-4 text-lg">Aplicação iniciada com sucesso!</p>
-    </div>
-  );
+  return null;
 }
 
 export default App;
